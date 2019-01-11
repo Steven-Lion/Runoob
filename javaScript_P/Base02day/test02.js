3bdd5229a5ca451eb78276dde89d71d69c5a36f9
Steven_lion <823961237@qq.com>
2019-01-11 12:16:26 +0800

    test02day

---
+++ javaScript_P/Base02day/test02.js
@@ -0,0 +1,20 @@
+function newtext(){
+
+    var name ;
+    name="hello1";
+    document.write(name);
+    document.write("</br>");
+    name="lala";//重新赋值.
+    document.write(name);
+
+}
+function showAlert(){
+   var j =  document.getElementById("Jhon");
+   window.alert(j);
+    if(j==fenife){
+        window.alert("你好，我是一个警告框！");
+    }
+    else{
+        window.alert(j);
+    }
+}
